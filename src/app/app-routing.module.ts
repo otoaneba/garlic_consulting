41234cf8d0d8508a3a617ee649071efbb7bc7620
Naoto Abe <naotoabe1@gmail.com>
2023-10-24 10:40:00 +0200

    add navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './view/parent-views/home-page/home-page.component';
+import { AboutUsComponent } from './view/parent-views/about-us/about-us.component';
 
 const routes: Routes = [];
 routes.push(
-  {path: '', component: HomePageComponent}
+  {path: '', component: HomePageComponent},
+  {path: 'about-us', component: AboutUsComponent}
 )
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './view/child-views/menu/menu.component';
 import { HomePageComponent } from './view/parent-views/home-page/home-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutUsComponent } from './view/parent-views/about-us/about-us.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    HomePageComponent
+    HomePageComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
